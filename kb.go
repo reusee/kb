291040a24614a04ca379ba0ae04b9bae1a862918
reus <reusee@gmail.com>
2018-09-06 20:21:51 +0800

    add double-tapping capslock to meta

--- kb.go
+++ kb.go
@@ -157,7 +157,7 @@ func main() {
 
 		type stateFunc func(ev *C.struct_input_event, raw []byte) bool
 
-		doubleShiftToCtrl := func() stateFunc {
+		doubleToModifier := func(key C.ushort, press, release []byte) stateFunc {
 			state := 0
 			var t time.Time
 			var code C.ushort
@@ -170,7 +170,7 @@ func main() {
 				}
 				switch state {
 				case 0:
-					if ev.code == C.KEY_LEFTSHIFT || ev.code == C.KEY_RIGHTSHIFT {
+					if ev.code == key {
 						state = 1
 						t = time.Now()
 						code = ev.code
@@ -186,15 +186,19 @@ func main() {
 				case 2:
 					state = 0
 					if time.Since(t) < interval {
-						writeEv(ctrlPress)
+						writeEv(press)
 						writeEv(raw)
-						writeEv(ctrlRelease)
+						writeEv(release)
 						return true
 					}
 				}
 				return false
 			}
-		}()
+		}
+
+		doubleShiftToCtrlLeft := doubleToModifier(C.KEY_LEFTSHIFT, ctrlPress, ctrlRelease)
+		doubleShiftToCtrlRight := doubleToModifier(C.KEY_RIGHTSHIFT, ctrlPress, ctrlRelease)
+		doubleCapslockToMeta := doubleToModifier(C.KEY_CAPSLOCK, metaPress, metaRelease)
 
 		shiftToMeta := func() stateFunc {
 			state := 0
@@ -243,7 +247,9 @@ func main() {
 			}
 			ev := (*C.struct_input_event)(unsafe.Pointer(&raw[0]))
 			for _, fn := range []stateFunc{
-				doubleShiftToCtrl,
+				doubleShiftToCtrlLeft,
+				doubleShiftToCtrlRight,
+				doubleCapslockToMeta,
 				shiftToMeta,
 			} {
 				if fn(ev, raw) {
