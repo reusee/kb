a850ef0f327dbc5cc7fb06d55f8bc92c223195a2
reus <reusee@gmail.com>
2018-09-30 15:41:00 +0800

    separate C codes

--- kb.go
+++ kb.go
@@ -2,36 +2,8 @@ package main
 
 /*
 #include <linux/uinput.h>
-#include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <stdio.h>
-
-unsigned long* get_mask(unsigned long fd) {
-	unsigned long *mask = malloc(
-		(KEY_MAX+(sizeof(unsigned long)*8)-1)/(sizeof(unsigned long)*8)
-	);
-	ioctl(fd, EVIOCGBIT(EV_KEY, KEY_MAX), mask);
-	return mask;
-}
-
-void set_mask(unsigned long fd, unsigned long *mask) {
-	int key;
-	for (key = KEY_RESERVED; key <= KEY_MAX; key++) {
-		if ((mask[key / (sizeof(unsigned long) * 8)] >> (key % (sizeof(unsigned long) * 8))) & 1) {
-			ioctl(fd, UI_SET_KEYBIT, key);
-		}
-	}
-}
-
-unsigned int eviocgbit(int a, int b) {
-	return EVIOCGBIT(a, b);
-}
-
-void pe() {
-	perror("error");
-}
-
+#include "kb.h"
 */
 import "C"
 
---
+++ kb.h
@@ -0,0 +1,4 @@
+unsigned long* get_mask(unsigned long fd);
+void set_mask(unsigned long fd, unsigned long *mask);
+unsigned int eviocgbit(int a, int b);
+void pe();
