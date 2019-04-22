8dc26ce717690aeef227094ec9313079ad28729f
reus <reusee@gmail.com>
2019-04-22 10:42:29 +0800

    refactor

--- kb.h
+++
@@ -1,4 +0,0 @@
-unsigned long* get_mask(unsigned long fd);
-void set_mask(unsigned long fd, unsigned long *mask);
-unsigned int eviocgbit(int a, int b);
-void pe();
--- main.go
+++ main.go
@@ -3,8 +3,34 @@ package main
 /*
 #include <linux/uinput.h>
 #include <sys/timerfd.h>
+#include <malloc.h>
 #include <string.h>
-#include "kb.h"
+
+unsigned long* get_mask(unsigned long fd) {
+	unsigned long *mask = malloc(
+		(KEY_MAX+(sizeof(unsigned long)*8)-1)/(sizeof(unsigned long)*8)
+	);
+	ioctl(fd, EVIOCGBIT(EV_KEY, KEY_MAX), mask);
+	return mask;
+}
+
+void set_mask(unsigned long fd, unsigned long *mask) {
+	int key;
+	for (key = KEY_RESERVED; key <= KEY_MAX; key++) {
+		if ((mask[key / (sizeof(unsigned long) * 8)] >> (key % (sizeof(unsigned long) * 8))) & 1) {
+			ioctl(fd, UI_SET_KEYBIT, key);
+		}
+	}
+}
+
+unsigned int eviocgbit(int a, int b) {
+	return EVIOCGBIT(a, b);
+}
+
+void pe() {
+	perror("error");
+}
+
 */
 import "C"
 
